2c3c401262d48512d412514cc2e06bd1a2ffdbef
Koosha Paridehpour <kooshapari@gmail.com>
2026-02-23 15:26:30 -0700

    fix: SDK type unification for handlers

--- sdk/auth/kilo.go
+++ sdk/auth/kilo.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/router-for-me/CLIProxyAPI/v6/pkg/llmproxy/auth/kilo"
-	"github.com/router-for-me/CLIProxyAPI/v6/internal/config"
+	"github.com/router-for-me/CLIProxyAPI/v6/pkg/llmproxy/config"
 	coreauth "github.com/router-for-me/CLIProxyAPI/v6/sdk/cliproxy/auth"
 )
 
